24a941ea33657f39e57853a99aa24c63e425a773
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-21 22:34:03 +0300

    Modified logging function

--- scripts/log.py
+++ scripts/log.py
@@ -1,15 +1,15 @@
 # Import packages for logging
-import log
-import log.handlers
+import logging
+import logging.handlers
 import os
 
 
 def logger(filename):
-    handler = log.handlers.WatchedFileHandler(
+    handler = logging.handlers.WatchedFileHandler(
     os.environ.get("LOGFILE", f"../logs/{filename}.log"))
-    formatter = log.Formatter(log.BASIC_FORMAT)
+    formatter = logging.Formatter(logging.BASIC_FORMAT)
     handler.setFormatter(formatter)
-    root = log.getLogger()
+    root = logging.getLogger()
     root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
     root.addHandler(handler)
     # logging.info("Testing Loggings")
