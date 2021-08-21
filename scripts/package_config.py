de7782fb39c7311dd8ee1d53ac97bb7faeba4ef4
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-21 03:55:29 +0300

    Added test data directory

--- scripts/package_config.py
+++ scripts/package_config.py
@@ -9,7 +9,15 @@ class Config:
   Tif_path = Data_path / "shp"
   Shp_path = Data_path / "tif"
   Image_path = Data_path / "img"
+
+  Test_data_path = Root_Path / "tests/test_data"
+  test_Laz_path = Test_data_path / "laz"
+  test_Tif_path = Test_data_path / "shp"
+  test_Shp_path = Test_data_path / "tif"
+  test_Image_path = Test_data_path / "img"
+
   remote_data_path = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/IA_FullState/ept.json"
   remote_data_directory = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/"
+  region_names = "usgs3dep_region_names"
 
 
